50a1833c47fc682cd0050f13e90afb62bcf64b50
Bruno Soares <brunolucassoares@gmail.com>
2021-02-15 11:29:13 -0300

    Ajuste no PWM

--- fan_control_auto.py
+++ fan_control_auto.py
@@ -6,14 +6,14 @@ import time
 import sys
 
 FAN_PIN = 14
-WAIT_TIME = 1
-FAN_MIN = 30
-PWM_FREQ = 100
+WAIT_TIME = 2
+FAN_MIN = 20
+PWM_FREQ = 30
 
-tempSteps = [45, 70]
-speedSteps = [0, 100]
+tempSteps = list(range(55, 70))
+speedSteps = list(range(0, 100))
 
-hyst = 1
+hyst = 2
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(FAN_PIN, GPIO.OUT, initial=GPIO.LOW)
@@ -26,10 +26,6 @@ fanSpeed = 0
 cpuTempOld = 0
 fanSpeedOld = 0
 
-if len(speedSteps) != len(tempSteps):
-    print("Numbers of temp steps and speed steps are different")
-    exit(0)
-
 try:
     while 1:
         cpuTempFile = open("/sys/class/thermal/thermal_zone0/temp", "r")
@@ -40,19 +36,17 @@ try:
             if cpuTemp < tempSteps[0]:
                 fanSpeed = speedSteps[0]
             elif cpuTemp >= tempSteps[len(tempSteps) - 1]:
-                fanSpeed = speedSteps[len(tempSteps) - 1]
+                fanSpeed = speedSteps[len(speedSteps) - 1]
             else:
                 for i in range(0, len(tempSteps) - 1):
                     if (cpuTemp >= tempSteps[i]) and (cpuTemp < tempSteps[i + 1]):
-                        fanSpeed = round((speedSteps[i + 1] - speedSteps[i])
-                                         / (tempSteps[i + 1] - tempSteps[i])
-                                         * (cpuTemp - tempSteps[i])
-                                         + speedSteps[i], 1)
-
+                        fanSpeed = speedSteps[int(round(len(speedSteps) * i / len(tempSteps)))]
             if fanSpeed != fanSpeedOld:
-                if (fanSpeed != fanSpeedOld and (fanSpeed >= FAN_MIN or fanSpeed == 0)):
+                if fanSpeed >= FAN_MIN:
                     fan.ChangeDutyCycle(fanSpeed)
-                    fanSpeedOld = fanSpeed
+                else:
+                    fan.ChangeDutyCycle(0)
+                fanSpeedOld = fanSpeed
             cpuTempOld = cpuTemp
 
         time.sleep(WAIT_TIME)
